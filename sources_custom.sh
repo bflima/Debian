77256992affdece249339bfc85d2ffeef2bebc9c
Bruno Lima <47477458+bflima@users.noreply.github.com>
2025-06-29 21:07:18 -0300

    Update sources_custom.sh

--- sources_custom.sh
+++ sources_custom.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
 # -----------------------------------------------------------------------------
-# NOME...............: sources_custom.sh
-# VERSÃO.............: 1.0.0
-# DESCRIÇÃO..........: Instala pacotes extras
+# NOME...............: bash_extra.sh
+# VERSÃO.............: 1.0.5
+# DESCRIÇÃO..........: Instala pacotes extras (contrib e non-free)
 # AUTOR..............: Bruno Lima
 # GITHUB.............: https://github.com/bflima
 # DATA DE CRIAÇÃO....: 28/06/2025
@@ -11,7 +11,7 @@
 #
 # -----------------------------------------------------------------------------
 # USO:
-#  ./sources_custom.sh
+#  ./bash_custom.sh
 
 set -euo pipefail
 
@@ -21,7 +21,11 @@ clear
 
 # Script homologado para distribuição debian
 OS_VERSION=$(grep -i "^id" /etc/os-release)
+OS_CODENANE=$(grep -i code /etc/os-release)
+
 [[ ${OS_VERSION##*=} == 'debian' ]] || { echo "Script configurado para a distribuição Debian" ; exit 1 ; }
+
+[[ ${OS_CODENANE##*=} == 'bookworm' ]] || { echo "Script configurado para a distribuição Debian" ; exit 1 ; }
 
 # Verificar permissão de root
 [[ "$(id -u)" -eq 0 ]] || { echo "ERRO: Este script precisa ser executado com privilégios de root (sudo)." ; exit 1 ; }
@@ -31,16 +35,20 @@ SOURCE_CUSTOM="/etc/apt/sources.list"
 cat > "$SOURCE_CUSTOM" << EOF
 #deb cdrom:[Debian GNU/Linux 12.8.0 _Bookworm_ - Official amd64 NETINST with firmware 20241109-11:04]/ bookworm contrib main non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free
-deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free
+deb http://deb.debian.org/debian/ ${OS_CODENANE##*=} main non-free-firmware contrib non-free
+deb-src http://deb.debian.org/debian/ ${OS_CODENANE##*=} main non-free-firmware contrib non-free
 
-deb http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free
-deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free
+deb http://security.debian.org/debian-security ${OS_CODENANE##*=}-security main non-free-firmware contrib non-free
+deb-src http://security.debian.org/debian-security ${OS_CODENANE##*=}-security main non-free-firmware contrib non-free
 
 # bookworm-updates, to get updates before a point release is made;
 # see https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_updates_and_backports
-deb http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free
-deb-src http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free
+deb http://deb.debian.org/debian/ ${OS_CODENANE##*=}-updates main non-free-firmware contrib non-free
+deb-src http://deb.debian.org/debian/ ${OS_CODENANE##*=}-updates main non-free-firmware contrib non-free
+
+# Backports allow you to install newer versions of software made available for this release
+deb http://deb.debian.org/debian ${OS_CODENANE##*=}-backports main non-free-firmware contrib non-free
+deb-src http://deb.debian.org/debian ${OS_CODENANE##*=}-backports main non-free-firmware contrib non-free
 
 # This system was installed using small removable media
 # (e.g. netinst, live or single CD). The matching "deb cdrom"
